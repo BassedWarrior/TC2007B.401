fce634f72a171682ec5ef28d09c3cd3a85e452e1
BassedWarrior <bassedwarrior724@outlook.com>
2024-09-18 18:12:59 -0600

    feat(frontend): Added EditButton component to list view of projects

--- frontend/src/proyectos.tsx
+++ frontend/src/proyectos.tsx
@@ -14,6 +14,7 @@ import {
     RadioButtonGroupInput,
     Show,
     SimpleShowLayout,
+    EditButton,
     Create,
     required,
 } from "react-admin";
@@ -39,6 +40,7 @@ export const ProyectosList = () => (
                 style: "currency",
                 currency: "MXN",
             }} />
+            <EditButton/>
         </Datagrid>
     </List>
 );
