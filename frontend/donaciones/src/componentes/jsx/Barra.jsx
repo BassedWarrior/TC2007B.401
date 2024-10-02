8dbddf109835379e9ed854967d9ebc2b03c57c73
julianRamirez <A01027743@tec.mx>
2024-10-02 00:08:32 -0600

    navbar finalizado

--- frontend/donaciones/src/componentes/jsx/Barra.jsx
+++ frontend/donaciones/src/componentes/jsx/Barra.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import SandersLogo from '../../logos/SandersLogo.jpeg';
+import './../css/Barra.css';
 
 const Barra = () => {
   const navigate = useNavigate();
 
   // Funciones para manejar la navegación
   const handleInicio = () => {
-    navigate('/inicio');
+    navigate('/');
   };
 
   const handleNosotros = () => {
@@ -17,11 +19,21 @@ const Barra = () => {
     navigate('/proyectos');
   };
 
+  const handleDonar = () => {
+    navigate('/donar');
+  };
+
   return (
-    <div>
-      <button onClick={handleInicio}>INICIO</button>
-      <button onClick={handleNosotros}>NOSOTROS</button>
-      <button onClick={handleProyectos}>PROYECTOS</button>
+    <div className='NavBar'>
+      <img src={SandersLogo} alt="Logo Sanders" className='LogoSanders' />
+
+      <div className='botones'>
+        <button className='boton' onClick={handleInicio}>INICIO</button>
+        <button className='boton' onClick={handleNosotros}>NOSOTROS</button>
+        <button className='boton' onClick={handleProyectos}>PROYECTOS</button>
+      </div>
+
+      <button className='boton donar' onClick={handleDonar}>DONAR AHORA</button> {/* Botón DONAR AHORA a la derecha */}
     </div>
   );
 };
--- frontend/donaciones/src/paginas/jsx/Inicio.jsx
+++ frontend/donaciones/src/paginas/jsx/Inicio.jsx
@@ -5,9 +5,7 @@ import { Link } from "react-router-dom";
 const Inicio = () => {
   return (
     <div>
-      <nav>
         <h1>ESTAS EN EL INICIO</h1>
-      </nav>
     </div>
   )
 }
