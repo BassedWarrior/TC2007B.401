f24c788df89f6db8bc33960f025ed7dc247b0ee4
julianRamirez <A01027743@tec.mx>
2024-10-02 08:49:35 -0600

    falta css, pero ya hay pagina de inicio

--- frontend/donaciones/src/paginas/jsx/Inicio.jsx
+++ frontend/donaciones/src/paginas/jsx/Inicio.jsx
@@ -1,12 +1,39 @@
 import React from 'react'
-import Barra from '../../componentes/jsx/Barra'
-import { Link } from "react-router-dom";
+
+import Captacion from '../../imagenes/inicio/captacion.png'
+import Cisternas from '../../imagenes/inicio/cisternas.png'
+import CuidarAgua from '../../imagenes/inicio/cuidarAgua.jpg'
 
 const Inicio = () => {
   return (
     <div>
-        <h1>ESTAS EN EL INICIO</h1>
-    </div>
+
+      <div className='PrimeraParte'>
+        <img src = {Captacion} alt="Captacion Agua" className='Captacion'></img>
+        <div className='Creemos'>
+          <h2>¿En qué creemos?</h2>
+          <p>Que dotar de agua potable a comunidades de bajos recursos disminuye las barreras de acceso al bienestar y mitiga los factores de riesgo a la salud.</p>
+        </div>
+      </div>
+
+      <div className='SegundaParte'>
+        <img src = {Cisternas} alt="Cisterna de agua" className='Cisterna'></img>
+        <div className='ComoHacemos'>
+          <h2>¿Cómo lo hacemos?</h2>
+          <p>Llevamos agua potable a todas las comunidades mediante sistemas de captación de agua pluvial y abasto de agua potable.</p>
+        </div>
+      </div>
+
+      <div className='TerceraParte'>
+      <img src = {CuidarAgua} alt="CuidarAgua" className='CuidarAgua'></img>
+        <div className='PorQue'>
+          <h2>¿Por qué lo hacemos?</h2>
+          <p>Disponer de agua en cantidad y condiciones suficientes, y tener un impacto directo en la calidad de vida y el desarrollo social.</p>  
+        </div>
+      </div>
+
+
+    </div>//main div
   )
 }
 
