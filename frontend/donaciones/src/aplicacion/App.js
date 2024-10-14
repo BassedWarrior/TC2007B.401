be11477639e842236800c868acd57771b9b0cb8d
julianRamirez <A01027743@tec.mx>
2024-10-13 19:42:16 -0600

    finalmente footer

--- frontend/donaciones/src/aplicacion/App.js
+++ frontend/donaciones/src/aplicacion/App.js
@@ -1,21 +1,28 @@
 import ReactDOM from 'react-dom';
 import {Route, Routes} from 'react-router-dom';
 
+import Barra from '../componentes/jsx/Barra';
+import PieAbajo from '../componentes/jsx/PieAbajo'
+
 import Inicio from '../paginas/jsx/Inicio'
 import Nosotros from '../paginas/jsx/Nosotros'
 import Proyectos from '../paginas/jsx/Proyectos'
-import Barra from '../componentes/jsx/Barra'
+
 
 
 function App() {
   return (
     <div className='Aplicacion'>
+        
         <Barra/>
-        <Routes>
-          <Route path='/' element={<Inicio />} />
-          <Route path='/nosotros' element={<Nosotros />} />
-          <Route path='/proyectos' element={<Proyectos />} />
-        </Routes>
+          <Routes>
+            <Route path='/' element={<Inicio />} />
+            <Route path='/nosotros' element={<Nosotros />} />
+            <Route path='/proyectos' element={<Proyectos />} />
+          </Routes>
+
+        <PieAbajo/>
+        
     </div>
   );
 }
---
+++ frontend/donaciones/src/componentes/jsx/PieAbajo.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import "../css/PieAbajo.css"
+
+const PieAbajo = () => {
+    console.log("PieAbajo renderizado");
+  return (
+    <div className='PieAbajo'>
+
+        <div className='Direccion'>
+            <p><strong>Dirección:</strong> Melchor Ocampo 193, Torre A, Piso 1, CP 11300 Col. Verónica Anzures</p>
+        </div>
+
+        <div className='Telefono'>
+            <p><strong>Teléfono:</strong> 55 52 08 11 11</p>
+        </div>
+
+        <div className='Correo'>
+            <p>
+                <strong>Correo:</strong> 
+                <a href="mailto:contacto@sanders.com.mx" className='CorreoLink'> contacto@sanders.com.mx</a>
+            </p>
+        </div>
+
+    </div>
+  )
+}
+
+export default PieAbajo;
