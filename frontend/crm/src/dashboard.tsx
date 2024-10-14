3e5a23e4c11daa132c415fec8d7ce3e5dbd44e27
paydelimon22 <valentina2212.gonzalez@gmail.com>
2024-10-14 13:04:11 -0600

    feat: added graph types, missing styling and connection to the backend script(issue 43)

--- frontend/crm/src/dashboard.tsx
+++ frontend/crm/src/dashboard.tsx
@@ -1,25 +1,38 @@
-// in src/Dashboard.js
 import * as React from "react";
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import CardHeader from '@mui/material/CardContent';
-import Grid from '@mui/material/CardContent';
+import CardHeader from '@mui/material/CardHeader';
+import Grid from '@mui/material/Grid'; // Correct import for Grid
 import { Title } from 'react-admin';
-import {IngresoMensual} from "./graphs/ingresoMensual";
-import {TipoDonacion} from "./graphs/tipoDonacion";
+import { IngresoMensual } from "./graphs/ingresoMensual";
+import { ProyectosMensual } from "./graphs/proyectosMensual";
+import { EstadosProyectos } from "./graphs/estadoProyectos";
 
 export const Dashboard = () => (
     <div>
-    <Card>
-        <CardContent>
-            {IngresoMensual()}
-        </CardContent>
-    </Card>
-    <Card>
-        <CardContent>
-            {TipoDonacion()}
-        </CardContent>
-    </Card>
+        <Title title="Dashboard" />
+        <Grid container spacing={1}>
+            <Grid item xs={12} sm={6} md={4}>
+                <Card>
+                    <CardContent>
+                        <IngresoMensual />
+                    </CardContent>
+                </Card>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+                <Card>
+                    <CardContent>
+                        <ProyectosMensual />
+                    </CardContent>
+                </Card>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+                <Card>
+                    <CardContent>
+                        <EstadosProyectos />
+                    </CardContent>
+                </Card>
+            </Grid>
+        </Grid>
     </div>
-
 );
--- frontend/crm/src/graphs/proyectosMensual.tsx
+++ frontend/crm/src/graphs/proyectosMensual.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Plot from 'react-plotly.js';
 
-export const TipoDonacion =()=>(
+export const ProyectosMensual =()=>(
   <Plot
     data={[{
         type: 'pie', 
