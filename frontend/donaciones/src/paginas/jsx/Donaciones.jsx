8f96751754f346587e398649a7c91345bde7324a
julianRamirez <A01027743@tec.mx>
2024-10-14 01:00:25 -0600

    formulario y estilo funcionando

--- frontend/donaciones/src/paginas/jsx/Donaciones.jsx
+++ frontend/donaciones/src/paginas/jsx/Donaciones.jsx
@@ -1,38 +1,46 @@
 import React from 'react';
-import '../css/Donaciones.css'
+import '../css/Donaciones.css';
 
-import {useForm} from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 
 const Donaciones = () => {
 
-    const {register, handleSubmit, formState: {errors}} = useForm();
+    const { register, handleSubmit, formState: { errors } } = useForm();
     console.log(errors);
-    const onSubmit = handleSubmit((data) => {console.log(data)})
-    
-  return (
-    <div className='Pagina'>
-        <form onSubmit={onSubmit}>
-            <label htmlFor='nombre'>Nombre: </label>
-            <input type='text' {...register("nombre", {required: true})} />
-            {errors.nombre && <span>Nombre es requerido</span>}
+    const onSubmit = handleSubmit((data) => { console.log(data); });
 
-            <label htmlFor='correo'>Correo electrónico: </label>
-            <input type='email' {...register("correo", {required: true})} />
-            {errors.correo && <span>Correo electrónico es requerido</span>}
-
-            <label htmlFor='cantidad'>Monto a donar: </label>
-            <input type='number' {...register("cantidad")} />
-
-            <div className="checkbox-container">
-                <input type='checkbox' id='terminos' {...register("terminos", {required: true})} />
-                <label htmlFor='terminos'>Acepto términos y condiciones</label>
-                {errors.terminos && <span>Debes aceptar los términos y condiciones</span>}
+    return (
+        <div className='Pagina'>
+            {/* Contenedor para las líneas animadas */}
+            <div className="lines">
+                <div className="line"></div>
+                <div className="line"></div>
+                <div className="line"></div>
             </div>
-            
-            <button type='submit'>Donar</button>
-        </form>
-    </div>
-  );
+
+            {/* Formulario de donaciones */}
+            <form onSubmit={onSubmit}>
+                <label htmlFor='nombre'>Nombre: </label>
+                <input type='text' {...register("nombre", { required: true })} />
+                {errors.nombre && <span>Nombre es requerido</span>}
+
+                <label htmlFor='correo'>Correo electrónico: </label>
+                <input type='email' {...register("correo", { required: true })} />
+                {errors.correo && <span>Correo electrónico es requerido</span>}
+
+                <label htmlFor='cantidad'>Monto a donar: </label>
+                <input type='number' {...register("cantidad")} />
+
+                <div className="checkbox-container">
+                    <input type='checkbox' id='terminos' {...register("terminos", { required: true })} />
+                    <label htmlFor='terminos'>Acepto términos y condiciones</label>
+                    {errors.terminos && <span>Debes aceptar los términos y condiciones</span>}
+                </div>
+
+                <button type='submit'>Donar</button>
+            </form>
+        </div>
+    );
 };
 
 export default Donaciones;
