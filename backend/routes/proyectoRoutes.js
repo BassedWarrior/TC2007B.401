d36a724d13cfb689ea0e5b0745960b9237671bd7
BassedWarrior <bassedwarrior724@outlook.com>
2024-10-14 09:31:24 -0600

    refactor(backend): Move resource control methods to separate files
    The control methods which interact with the database are separated into
    their own files, improving modularity and ensuring that the
    implementation details are left separate from the routing functionality.
    Hence the routes have only limited access to the database and don't
    interact with it directly. Making it easier to later add middleware.

--- backend/routes/proyectoRoutes.js
+++ backend/routes/proyectoRoutes.js
@@ -4,134 +4,28 @@
 
 const express = require("express");  // Módulo para generar los endpoints.
 const router = express.Router();  // Gerente de rutas y de endpoints.
-// Esquema de proyecto de la base de datos.
-const Proyecto = require("../models/Proyecto");
+// Controlador de recurso en la base de datos.
+const proyectoController = require("../controllers/proyectoController");
 
 
 // Endpoints de distintos métodos HTTP para la ruta raiz de este recurso.
 // Referente a la totalidad de los proyectos en la base de datos.
 router.route("/")
     // HTTP GET: Obtener todos los proyectoes de la base de datos.
-    .get(async (req, res) => {
-        try {
-            const proyectos = await Proyecto.find();
-            const proyectosWithId = proyectos.map(proyecto => ({
-                id: proyecto._id,
-                nombre: proyecto.nombre,
-                descripcion: proyecto.descripcion,
-                inicio: proyecto.inicio,
-                fin: proyecto.fin,
-                estado: proyecto.estado,
-                presupuesto: proyecto.presupuesto,
-                objetivo: proyecto.objetivo,
-            }));
-            res.set('X-Total-Count', proyectos.length);
-            res.json(proyectosWithId);
-        } catch (err) {
-            res.status(500).json({ error: 'Error al obtener los proyectos' });
-        }
-    })
+    .get(proyectoController.getAllProyectos)
     // HTTP POST: Agregar una proyecto a la base de datos.
-    .post(async (req, res) => {
-        try {
-            const newProyecto = new Proyecto({
-                nombre: req.body.nombre,
-                descripcion: req.body.descripcion,
-                inicio: req.body.inicio,
-                fin: req.body.fin,
-                estado: req.body.estado,
-                presupuesto: req.body.presupuesto,
-                objetivo: req.body.objetivo,
-            });
-            const savedProyecto = await newProyecto.save();
-            res.status(201).json({
-                id: savedProyecto._id,
-                nombre: savedProyecto.nombre,
-                descripcion: savedProyecto.descripcion,
-                inicio: savedProyecto.inicio,
-                fin: savedProyecto.fin,
-                estado: savedProyecto.estado,
-                presupuesto: savedProyecto.presupuesto,
-                objetivo: savedProyecto.objetivo,
-            });
-        } catch (err) {
-            res.status(500).json({ error: 'Error al crear el proyecto' });
-        }
-    });
+    .post(proyectoController.createProyecto);
 
 
 // Endpoints de distintos métodos HTTP para la ruta a un objeto específico.
 // Referente a una solo proyecto de la base de datos.
 router.route("/:id")
     // HTTP GET: Obtener un solo registro de proyecto de la base de datos.
-    .get(async (req, res) => {
-        try {
-            const { id } = req.params;
-            const proyecto = await Proyecto.findById(id);
-
-            if (!proyecto) {
-                return res.status(404).json(
-                    { error: 'Proyecto no encontrado' }
-                );
-            }
-
-            const proyectoConId = {
-                id: proyecto._id,
-                nombre: proyecto.nombre,
-                descripcion: proyecto.descripcion,
-                inicio: proyecto.inicio,
-                fin: proyecto.fin,
-                estado: proyecto.estado,
-                presupuesto: proyecto.presupuesto,
-                objetivo: proyecto.objetivo,
-            };
-
-            res.json(proyectoConId);
-        } catch (err) {
-            res.status(500).json({ error: 'Error al buscar el proyecto' });
-        }
-    })
+    .get(proyectoController.getProyectoById)
     // HTTP PUT: Actualizar un solo registro de proyecto de la base de datos.
-    .put(async (req, res) => {
-        try {
-            const updatedProyecto = await Proyecto.findByIdAndUpdate(
-                req.params.id, req.body, { new: true }
-            );
-            if (!updatedProyecto) {
-                return res.status(404).json(
-                    { error: 'Proyecto no encontrado' }
-                );
-            };
-            res.json({
-                id: updatedProyecto._id,
-                nombre: updatedProyecto.nombre,
-                descripcion: updatedProyecto.descripcion,
-                inicio: updatedProyecto.inicio,
-                fin: updatedProyecto.fin,
-                estado: updatedProyecto.estado,
-                presupuesto: updatedProyecto.presupuesto,
-                objetivo: updatedProyecto.objetivo,
-            });
-        } catch (err) {
-            res.status(500).json({ error: 'Error al actualizar el proyecto' });
-        }
-    })
+    .put(proyectoController.updateProyectoById)
     // HTTP DELETE: Eliminar un solo registro de proyecto de la base de datos.
-    .delete(async (req, res) => {
-        try {
-            const deletedProyecto = await Proyecto.findByIdAndDelete(
-                req.params.id
-            );
-            if (!deletedProyecto) {
-                return res.status(404).json(
-                    { error: 'Proyecto no encontrado' }
-                );
-            };
-            res.json({ message: 'Proyecto eliminado' });
-        } catch (err) {
-            res.status(500).json({ error: 'Error al eliminar el proyecto' });
-        }
-    });
+    .delete(proyectoController.deleteProyectoById);
 
 
 module.exports = router;
