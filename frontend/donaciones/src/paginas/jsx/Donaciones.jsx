23541cca92769af535c8b43e93550c1bdc2075e9
julianRamirez <A01027743@tec.mx>
2024-10-14 00:25:28 -0600

    una version mejorada del formulario

--- frontend/donaciones/src/paginas/jsx/Donaciones.jsx
+++ frontend/donaciones/src/paginas/jsx/Donaciones.jsx
@@ -1,36 +1,36 @@
 import React from 'react';
-import './../css/Donaciones.css';
+import '../css/Donaciones.css'
+
+import {useForm} from 'react-hook-form';
 
 const Donaciones = () => {
+
+    const {register, handleSubmit, formState: {errors}} = useForm();
+    console.log(errors);
+    const onSubmit = handleSubmit((data) => {console.log(data)})
+    
   return (
     <div className='Pagina'>
+        <form onSubmit={onSubmit}>
+            <label htmlFor='nombre'>Nombre: </label>
+            <input type='text' {...register("nombre", {required: true})} />
+            {errors.nombre && <span>Nombre es requerido</span>}
 
-        <div className="lines">
-            <div className="line"></div>
-            <div className="line"></div>
-            <div className="line"></div>
-        </div>
+            <label htmlFor='correo'>Correo electrónico: </label>
+            <input type='email' {...register("correo", {required: true})} />
+            {errors.correo && <span>Correo electrónico es requerido</span>}
 
+            <label htmlFor='cantidad'>Monto a donar: </label>
+            <input type='number' {...register("cantidad")} />
 
-        <div className='Formulario'>
-            <form>
-                <label htmlFor='nombre'> Nombre: </label>
-                <input type='text'/>
-
-                <label htmlFor='correo'>Correo electrónico: </label>
-                <input type='email'/>
-
-                <label htmlFor='cantidad'>Monto a donar: </label>
-                <input type='number'/>
-
-                <div className="checkbox-container">
-                    <input type='checkbox' id='terminos'/>
-                    <label htmlFor='terminos'>Acepto términos y condiciones</label>
-                </div>
-
-                <button>Donar</button>
-            </form>
-        </div>
+            <div className="checkbox-container">
+                <input type='checkbox' id='terminos' {...register("terminos", {required: true})} />
+                <label htmlFor='terminos'>Acepto términos y condiciones</label>
+                {errors.terminos && <span>Debes aceptar los términos y condiciones</span>}
+            </div>
+            
+            <button type='submit'>Donar</button>
+        </form>
     </div>
   );
 };
